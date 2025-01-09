d1d44b873786232c285e1dc8e05907901cdfa1a8
Trevor <me@trev.app>
2025-01-09 18:52:39 -0500

    Fix FFMPEG error output; fix parameter encoding

--- src/Lidarr.Plugin.Tidal/FFMPEG.cs
+++ src/Lidarr.Plugin.Tidal/FFMPEG.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace NzbDrone.Core.Plugins;
 
@@ -7,28 +8,28 @@ internal class FFMPEG
 {
     public static string[] ProbeCodecs(string filePath)
     {
-        var (exitCode, output) = Call("ffprobe", $"-select_streams a -show_entries stream=codec_name:stream_tags=language -of default=nk=1:nw=1 \"{filePath}\"");
+        var (exitCode, output, errorOutput, a) = Call("ffprobe", $"-select_streams a -show_entries stream=codec_name:stream_tags=language -of default=nk=1:nw=1 {EncodeParameterArgument(filePath)}");
         if (exitCode != 0)
-            throw new FFMPEGException($"Probing codecs failed\n{output}");
+            throw new FFMPEGException($"Probing codecs failed\n{a}\n{errorOutput}");
 
         return output.Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
     }
 
     public static void ConvertWithoutReencode(string input, string output)
     {
-        var (exitCode, _) = Call("ffmpeg", $"-i \"{input}\" -vn -acodec copy \"{output}\"");
+        var (exitCode, _, errorOutput, a) = Call("ffmpeg", $"-i {EncodeParameterArgument(input)} -vn -acodec copy {EncodeParameterArgument(output)}");
         if (exitCode != 0)
-            throw new FFMPEGException($"Conversion without re-encode failed\n{output}");
+            throw new FFMPEGException($"Conversion without re-encode failed\n{a}\n{errorOutput}");
     }
 
     public static void Reencode(string input, string output, int bitrate)
     {
-        var (exitCode, _) = Call("ffmpeg", $"-i \"{input}\" -b:a {bitrate}k \"{output}\"");
+        var (exitCode, _, errorOutput, a) = Call("ffmpeg", $"-i {EncodeParameterArgument(input)} -b:a {bitrate}k {EncodeParameterArgument(output)}");
         if (exitCode != 0)
-            throw new FFMPEGException($"Re-encoding failed\n{output}");
+            throw new FFMPEGException($"Re-encoding failed\n{a}\n{errorOutput}");
     }
 
-    private static (int exitCode, string output) Call(string executable, string arguments)
+    private static (int exitCode, string output, string errorOutput, string arg) Call(string executable, string arguments)
     {
         using var proc = new Process()
         {
@@ -44,9 +45,20 @@ internal class FFMPEG
 
         proc.Start();
         var output = proc.StandardOutput.ReadToEnd();
+        var errorOutput = proc.StandardError.ReadToEnd();
         proc.WaitForExit(60000);
 
-        return (proc.ExitCode, output);
+        return (proc.ExitCode, output, errorOutput, arguments);
+    }
+
+    private static string EncodeParameterArgument(string original)
+    {
+        if (string.IsNullOrEmpty(original))
+            return original;
+
+        var value = Regex.Replace(original, @"(\\*)" + "\"", @"$1\$0");
+        value = Regex.Replace(value, @"^(.*\s.*?)(\\*)$", "\"$1$2$2\"");
+        return value;
     }
 }
 
