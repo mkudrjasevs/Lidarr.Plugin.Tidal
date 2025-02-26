5042de9d4fb32ba381f5bac7d35f823cacfdeb2f
Trevor <me@trev.app>
2025-02-26 11:57:00 -0500

    Change API key
    Uses Android Auto which supports Normal, High, HiFi, and Master

--- src/TidalSharp/Data/Globals.cs
+++ src/TidalSharp/Data/Globals.cs
@@ -10,8 +10,8 @@ internal static class Globals
 
     public const string PKCE_URI_REDIRECT = "https://tidal.com/android/login/auth";
 
-    public const string CLIENT_ID = "zrU4bXHjVVnc.2AtmDPdog4et";
-    public const string CLIENT_SECRET = "V\x8cJ\x81K|hyD\x84Fyq\x88J\x81P.qZv\x86s\x85P}V\x80N~BV6ukXTJmlvbttP7wlMlrc72se4=";
+    public const string CLIENT_ID = "zU4XHVVkc2tDPo4t";
+    public const string CLIENT_SECRET = "VJKhDFqJPqvsPVNBV6ukXTJmwlvbttP7wlMlrc72se4=";
 
     public const string CLIENT_ID_PKCE = "6BDSRdpK9hqEBTgU";
     public const string CLIENT_SECRET_PKCE = "xeuPmY7nbpZ9IIbLAcQ93shka1VNheUAqN6IcszjTG8=";
